0cc097c0feeecd499e508990a1ab00a270ca4fdc
Max75020 <maxime.duplaissy@gmail.com>
2024-09-30 13:42:03 +0200

    Refactorisation du code pour la classe AdresseProcessor et UserPasswordHasher

--- src/State/AdresseProcessor.php
+++ src/State/AdresseProcessor.php
@@ -23,13 +23,13 @@ class AdresseProcessor implements ProcessorInterface
 	{
 		if ($data instanceof Adresse) {
 			$currentUser = $this->security->getUser();
-	
+
 			// Cas pour un utilisateur standard (non administrateur)
 			if (!$this->security->isGranted('ROLE_ADMIN') && $data->getUtilisateur() === null) {
 				// Associer automatiquement l'utilisateur connecté
 				$data->setUtilisateur($currentUser);
 			}
-	
+
 			// Cas pour un administrateur
 			if ($this->security->isGranted('ROLE_ADMIN')) {
 				// Récupère l'utilisateur spécifié par l'administrateur
@@ -40,11 +40,11 @@ class AdresseProcessor implements ProcessorInterface
 				}
 			}
 		}
-	
+
 		// Persister les données dans la base
 		$this->entityManager->persist($data);
 		$this->entityManager->flush();
-	
+
 		return $data;
 	}
 }
--- src/State/UserPasswordHasher.php
+++ src/State/UserPasswordHasher.php
@@ -9,23 +9,22 @@ use App\Entity\Utilisateur;
 
 final readonly class UserPasswordHasher implements ProcessorInterface
 {
-public function __construct(
-private ProcessorInterface $processor,
-private UserPasswordHasherInterface $passwordHasher
-) {
-}
+	public function __construct(
+		private ProcessorInterface $processor,
+		private UserPasswordHasherInterface $passwordHasher
+	) {}
 
-public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): Utilisateur
-{
-if (!$data->getPassword()) {
-return $this->processor->process($data, $operation, $uriVariables, $context);
-}
+	public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): Utilisateur
+	{
+		if (!$data->getPassword()) {
+			return $this->processor->process($data, $operation, $uriVariables, $context);
+		}
 
-// Hash the password
-$hashedPassword = $this->passwordHasher->hashPassword($data, $data->getPassword());
-$data->setPassword($hashedPassword);
-$data->eraseCredentials();
+		// Hash the password
+		$hashedPassword = $this->passwordHasher->hashPassword($data, $data->getPassword());
+		$data->setPassword($hashedPassword);
+		$data->eraseCredentials();
 
-return $this->processor->process($data, $operation, $uriVariables, $context);
-}
+		return $this->processor->process($data, $operation, $uriVariables, $context);
+	}
 }
