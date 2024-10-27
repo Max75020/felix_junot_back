24a58f741bd24cabae00faa54c6b99b8ca5ac86d
max-advisto <mduplaissy@advisto.fr>
2024-10-27 20:34:04 +0100

    Refresh Token OK

--- src/Entity/RefreshToken.php
+++ src/Entity/RefreshToken.php
@@ -8,4 +8,5 @@ use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
 #[ORM\Entity]
 #[ORM\Table(name: 'refresh_tokens')]
 class RefreshToken extends BaseRefreshToken
-{}
+{
+}
